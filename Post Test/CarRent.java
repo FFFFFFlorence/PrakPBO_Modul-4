d22fb2de47ea9306763ef9fc7d6e7c6afdef6f8c
FFFFFFFlorence <regislake809@gmail.com>
2025-03-20 23:40:11 +0700

    first commit

---
+++ Post Test/CarRent.java
@@ -0,0 +1,104 @@
+public class CarRent {
+    private String noPlat;
+    private String merk;
+    private double hargaSewaPerHari;
+    private boolean kesediaanSewa = true;
+
+    public CarRent(String noPlat, String merk, double hargaSewaPerHari, boolean kesediaanSewa) {
+        this.noPlat = noPlat;
+        this.merk = merk;
+        this.hargaSewaPerHari = hargaSewaPerHari;
+        this.kesediaanSewa = kesediaanSewa;
+    }
+
+    public CarRent() {
+        this("B 0000 AAA", "Toyota Avanza", 250000, true);
+    }
+
+    public void info() {
+        System.out.println("No Plat\t\t    : " + noPlat);
+        System.out.println("Merk\t\t    : " + merk);
+        System.out.println("Harga Sewa Per Hari : " + utility.mataUang(hargaSewaPerHari));
+        System.out.println("Kesediaan Sewa\t    : " + kesediaanSewa);
+    }
+
+    void infoSewa() {
+        if (kesediaanSewa == true) {
+            System.out.println("Mobil dengan no plat " + noPlat + " telah disewa");
+            kesediaanSewa = false;
+        } else {
+            System.out.println("Mobil dengan no plat " + noPlat + " tidak tersedia");
+        }
+    }
+
+    public class pelanggan {
+        private String namaPenyewa;
+        private String noKTP_Penyewa;
+        private String noHP_Penyewa;
+    
+        public pelanggan(String namaPenyewa, String noKTP_Penyewa, String noHP_Penyewa) {
+            this.namaPenyewa = namaPenyewa;
+            this.noKTP_Penyewa = noKTP_Penyewa;
+            this.noHP_Penyewa = noHP_Penyewa;
+        }
+
+        public void dataPelanggan() {
+            System.out.println("Nama Penyewa\t: " + namaPenyewa);
+            System.out.println("No KTP Penyewa\t: " + noKTP_Penyewa);
+            System.out.println("No HP Penyewa\t: " + noHP_Penyewa);
+        }
+    }
+
+    public class Sewa {
+        private pelanggan penyewa;
+        private CarRent mobil;
+        private int lamaSewa;
+        private double totalBiaya;
+        private boolean statusSewa = false;
+
+        public Sewa(pelanggan penyewa, CarRent mobil, int lamaSewa) {
+            this.penyewa = penyewa;
+            this.mobil = mobil;
+            this.lamaSewa = lamaSewa;
+            penyewaan();
+        }    
+        
+        private void penyewaan() {
+            if (mobil.kesediaanSewa) {
+                System.out.println("\n================= Car's Sewa =================");
+                mobil.infoSewa();
+                totalBiaya = (lamaSewa * mobil.hargaSewaPerHari) - utility.diskon(lamaSewa, mobil.hargaSewaPerHari);
+                statusSewa = true;
+                mobil.kesediaanSewa = false;
+            } else {
+                System.out.println("\n================= Car's Sewa =================");
+                mobil.infoSewa();
+                statusSewa = false;
+            }
+        }
+        
+        public void dataSewa() {
+            System.out.println("==============================================");
+            penyewa.dataPelanggan();
+            System.out.println("Lama Sewa\t: " + lamaSewa + " hari");
+            System.out.println("Total Biaya\t: " + utility.mataUang(totalBiaya));
+            System.out.println("Status Sewa\t: " + statusSewa);
+            System.out.println("==============================================");
+        }
+    }
+
+    public class utility {
+        public static double diskon(int lamaSewa , double hargaSewaPerHari) {
+            if (lamaSewa > 5) {
+                return (hargaSewaPerHari * lamaSewa) * 0.1;
+            } else {
+                return 0;
+            }
+        }
+
+        public static String mataUang (double harga) {
+            return "Rp" + (int)harga;
+        }
+    }
+}
+
---
+++ Post Test/Main.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        CarRent car0 = new CarRent("B 2189 ZFB", "Honda Brio", 150000, true); 
+        CarRent car1 = new CarRent("B 1753 AGH", "Toyota Avanza", 230000, false);
+        CarRent car2 = new CarRent("B 8675 CTR", "Honda Jazz", 250000, true);
+        CarRent car3 = new CarRent("B 4212 ETS", "Suzuki Ertiga", 360000, true);
+        CarRent car4 = new CarRent("B 2353 FNG", "Daihatsu Xenia", 350000, true);
+        CarRent car5 = new CarRent("B 6453 SRT", "Mitsubishi Xpander", 480000, true);
+        CarRent car6 = new CarRent("B 6734 TKU", "Toyota Kijang", 450000, true);
+        CarRent car7 = new CarRent("B 5473 HRT", "Honda CR-V", 570000, true);
+        CarRent car8 = new CarRent("B 6536 TGE", "Suzuki Jimny", 590000, true);
+        CarRent car9 = new CarRent("B 8584 VSD", "Daihatsu Terios", 680000, true);
+        
+        System.out.println("\n========= WELCOME TO CAR's CAR =========");
+        System.out.println("\n====== Daftar Mobil yang Tersedia ======");
+        car0.info();
+        System.out.println("========================================");
+        car1.info();
+        System.out.println("========================================");
+        car2.info();
+        System.out.println("========================================");
+        car3.info();
+        System.out.println("========================================");
+        car4.info();
+        System.out.println("========================================");
+        car5.info();
+        System.out.println("========================================");
+        car6.info();
+        System.out.println("========================================");
+        car7.info();
+        System.out.println("========================================");
+        car8.info();
+        System.out.println("========================================");
+        car9.info();
+        System.out.println("========================================");
+        
+        CarRent.pelanggan pelanggan1 = car0.new pelanggan("Asep", "1234567890", "081234567890");
+        CarRent.pelanggan pelanggan2 = car1.new pelanggan("Budi", "0987654321", "089876543210");
+        CarRent.pelanggan pelanggan3 = car2.new pelanggan("Caca", "0987123456", "089871234560");
+        CarRent.pelanggan pelanggan4 = car3.new pelanggan("Dedi", "0987123456", "089871234560");
+        CarRent.pelanggan pelanggan5 = car4.new pelanggan("Eca", "0987123456", "089871234560");
+        CarRent.pelanggan pelanggan6 = car5.new pelanggan("Fafa", "0987123456", "089871234560");
+
+        CarRent.Sewa sewa1 = car0.new Sewa(pelanggan1, car5, 8);
+        sewa1.dataSewa();
+        CarRent.Sewa sewa2 = car1.new Sewa(pelanggan2, car1, 7);
+        sewa2.dataSewa();
+        CarRent.Sewa sewa3 = car2.new Sewa(pelanggan3, car7, 5);
+        sewa3.dataSewa();
+        CarRent.Sewa sewa4 = car3.new Sewa(pelanggan4, car3, 9);
+        sewa4.dataSewa();
+        CarRent.Sewa sewa5 = car4.new Sewa(pelanggan5, car4, 6);
+        sewa5.dataSewa();
+        CarRent.Sewa sewa6 = car5.new Sewa(pelanggan6, car5, 8);
+        sewa6.dataSewa();
+        System.out.println();
+    }
+}
